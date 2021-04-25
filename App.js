ca8bdc77846469763c3f12f4baaae3a36def59f6
Melvin <31448155+melvio@users.noreply.github.com>
2021-04-25 12:58:25 +0200

    tried to merge different repos

--- App.js
+++ App.js
@@ -1,112 +1,161 @@
 import React, {useState} from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import {
+    KeyboardAvoidingView,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Keyboard,
+    ScrollView
+} from 'react-native';
 import Task from './components/Task';
+import {render} from "react-dom";
+import Calendar from "react-calendar";
+import reportWebVitals from "./reportWebVitals";
+import 'react-calendar/dist/Calendar.css';
 
 export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+    const [task, setTask] = useState();
+    const [taskItems, setTaskItems] = useState([]);
 
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task]);
+        setTask(null);
+    };
 
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy)
-  }
+    const completeTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy);
+    };
 
-  return (
-    <View style={styles.container}>
-      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1
-        }}
-        keyboardShouldPersistTaps='handled'
-      >
+    function getView() {
+        const [date, setDate] = useState(new Date());
 
-      {/* Today's Tasks */}
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-        <View style={styles.items}>
-          {/* This is where the tasks will go! */}
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                  <Task text={item} /> 
-                </TouchableOpacity>
-              )
-            })
-          }
-        </View>
-      </View>
-        
-      </ScrollView>
+        const onChange = date => {
+            setDate(date);
+        };
 
-      {/* Write a task */}
-      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
-      <KeyboardAvoidingView 
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-      
-    </View>
-  );
+        return (
+            <View style={styles.container}>
+                {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+                <ScrollView
+                    contentContainerStyle={{
+                        flexGrow: 1
+                    }}
+                    keyboardShouldPersistTaps="handled"
+                >
+
+                    {/* Today's Tasks */}
+                    <View style={styles.tasksWrapper}>
+                        <Text style={styles.sectionTitle}>Today's tasks</Text>
+                        <View style={styles.items}>
+                            {/* This is where the tasks will go! */}
+                            {
+                                taskItems.map((item, index) => {
+                                    return (
+                                        <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                                            <Task text={item}/>
+                                        </TouchableOpacity>
+                                    );
+                                })
+                            }
+                        </View>
+                    </View>
+
+                </ScrollView>
+
+                {/* Write a task */}
+                {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                    style={styles.writeTaskWrapper}
+                >
+                    <TextInput style={styles.input} placeholder={'Write a task'} value={task}
+                               onChangeText={text => setTask(text)}/>
+                    <TouchableOpacity onPress={() => handleAddTask()}>
+                        <View style={styles.addWrapper}>
+                            <Text style={styles.addText}>+</Text>
+                        </View>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
+                <div>
+                    <Calendar showWeekNumbers onChange={onChange} value={date}/>
+                    {date.toString()}
+                </div>
+
+            </View>
+        );
+    }
+
+    return getView();
 }
 
+// const ADHDCalendar = () => {
+//     const [date, setDate] = useState(new Date());
+//
+//     const onChange = date => {
+//         setDate(date);
+//     };
+//
+//     return (
+//         <div>
+//             <Calendar showWeekNumbers onChange={onChange} value={date}/>
+//             {console.log(date.toString())}
+//             {date.toString()}
+//         </div>
+//     );
+// };
+//
+//
+// render(<ADHDCalendar/>, document.querySelector("#root"));
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E8EAED',
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-  },
-  addText: {},
+    container: {
+        flex: 1,
+        backgroundColor: '#E8EAED',
+    },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    items: {
+        marginTop: 30,
+    },
+    writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+        width: 250,
+    },
+    addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+    },
+    addText: {},
 });
+
+reportWebVitals(console.log);
